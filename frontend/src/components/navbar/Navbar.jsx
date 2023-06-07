8dabad2568b0d851de20293b15d00ff42912fc31
krishnadev7 <krishnadevv12@gmail.com>
2023-06-07 21:39:09 +0530

    changed fiverr logo

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import fiverLogo from '../../assets/fiver-logo.png'
+import fiverLogo from '../../assets/Fiverr New.png'
 
 const Navbar = () => {
 
@@ -20,7 +20,7 @@ const Navbar = () => {
     <div className={ active ? 'flex flex-col items-center bg-white transition duration-500 ease-in-out sticky top-0' : 'flex flex-col items-center bg-red-800 transition duration-500 ease-in-out sticky top-0 '}>
       <div className='w-11/12 flex justify-between py-5 px-0'>
         <div>
-          <img className='h-7 w-20' src={fiverLogo} alt="fiver-logo"  />
+          <img className='h-24 w-36' src={fiverLogo} alt="fiver-logo"  />
         </div>
         <div className={active ? 'flex gap-6 font-medium items-center text-black' : 'flex gap-6 text-white font-medium items-center'}>
           <span>Fiverr Business</span>
