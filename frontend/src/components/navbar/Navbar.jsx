fad40b824d884a475ac938f8abc2dd8b1130897c
krishnadev7 <krishnadevv12@gmail.com>
2023-06-08 11:40:48 +0530

    completed react menu with useState

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import fiverLogo from '../../assets/Fiverr New.png'
+import userPic from '../../assets/user-picture.png'
 
 const Navbar = () => {
 
   const [active,setActive] = useState(false);
+  const [open,setOpen] = useState(false)
   
   const isActive = () => {
     window.scrollY > 0 ? setActive(true) : setActive(false);
@@ -16,22 +18,45 @@ const Navbar = () => {
     }
   },[])
 
+  const currentUser = {
+    id: 1,
+    userName: 'john doe',
+    isSeller: true
+  }
+
   return (
     <div className={ active ? 'flex flex-col items-center bg-white transition duration-500 ease-in-out sticky top-0' : 'flex flex-col items-center bg-red-800 transition duration-500 ease-in-out sticky top-0 '}>
-      <div className='w-11/12 flex justify-between py-5 px-0'>
+      <div className='w-11/12 flex justify-between py-1 px-0'>
         <div>
           <img className='h-24 w-36' src={fiverLogo} alt="fiver-logo"  />
         </div>
         <div className={active ? 'flex gap-6 font-medium items-center text-black' : 'flex gap-6 text-white font-medium items-center'}>
           <span>Fiverr Business</span>
           <span>Explore</span>
-          <span>Become a Seller</span>
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
           <span>
             Sign in
           </span>
-          <button className={active? 'py-1 px-4 text-green-600 rounded border border-solid border-green-600 hover:border-green-600 hover:bg-green-600 hover:text-white' :'py-1 px-4 text-white rounded border border-solid border-white hover:border-green-600 hover:bg-green-600'}>
+          {!currentUser && <button className={active? 'py-1 px-4 text-green-600 rounded border border-solid border-green-600 hover:border-green-600 hover:bg-green-600 hover:text-white' :'py-1 px-4 text-white rounded border border-solid border-white hover:border-green-600 hover:bg-green-600'}>
             join
-          </button>
+          </button>}
+          {currentUser && (
+            <div className='flex gap-2 items-center relative cursor-pointer ' onClick={() => setOpen(!open)}>
+              <img src={userPic} alt="userPic" className='h-8 w-8 rounded-md object-cover' />
+              <span>{currentUser?.userName}</span>
+            { open && <div className='absolute top-12 right-0 p-5 bg-white rounded-lg border-2 border-solid border-slate-200 flex flex-col gap-2 w-48 text-slate-600 font-light'>
+                {currentUser?.isSeller && (
+                  <>
+                  <span>Gigs</span>
+                  <span>Add new Gig</span>
+                  </>
+                )}
+                <span>Orders</span>
+                <span>Messages</span>
+                <span>Logout</span>
+              </div>}
+            </div>
+          )}
         </div>
       </div>
 
