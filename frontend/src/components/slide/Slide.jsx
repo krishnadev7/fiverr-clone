acbb97843c39a5e8bd909ccd0c5d46acc9a80d1b
krishnadev7 <krishnadevv12@gmail.com>
2023-06-20 19:56:37 +0530

    completed infinite-react-slider

--- frontend/src/components/slide/Slide.jsx
+++ frontend/src/components/slide/Slide.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import Slider from 'infinite-react-carousel';
 
-function Slide() {
+
+function Slide({children,slidesToShow,arrowsScroll}) {
   return (
-    <div>Slide</div>
-  )
+    <div className="flex justify-center pt-24 pb-0">
+      <div className="w-11/12">
+        <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll} >
+         {children}
+        </Slider>
+      </div>
+    </div>
+  );
 }
 
-export default Slide
+export default Slide;
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Featured from "../../components/featured/Featured";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
+import Slide from "../../components/slide/Slide";
+import {cards} from '../../data';
+import CatCard from '../../components/catCard/CatCard'
 
 const Home = () => {
   return (
     <div>
       <Featured />
       <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map((card) => (
+          <CatCard item={card} key={card.id}/>
+        ))}
+      </Slide>
     </div>
   );
 };
